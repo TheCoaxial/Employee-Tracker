857b67cb94773cdae4ab0e666a1c9b6aeb60c66c
Coaxial <thecoaxial@gmail.com>
2021-01-18 13:53:21 -0500

    Built inquirer function with prompts and switch statement skeleton

--- index.js
+++ index.js
@@ -17,3 +17,59 @@ const connection = mysql.createConnection({
     database: "employee_db"
 });
 
+// connection.connect goes here
+
+
+begin = () => {
+
+    inquirer
+        .prompt({
+            name: "doWhat",
+            type: "list",
+            message: "what would you like to do?",
+            choices: [
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                "View Employees",
+                "View Department",
+                "View Role",
+                "Update Employee Roles",
+                "Quit"
+            ]
+
+        })
+        .then(function(answer){
+            switch (answer.doWhat){
+                case "Add Employee":
+                    // Add Employee function
+                    break;
+                case "Add Department":
+                    // Add Department Function
+                    break;
+                case "Add Role":
+                    // Add Role Function
+                    break;
+                case "View Employees":
+                    // Add Viewemployees function
+                    break;
+                case "View Department":
+                    // Add view Department function
+                    break;
+                case "View Role":
+                    // Add view role function
+                    break;
+                case "Update Employee Roles":
+                    // Add update employee function
+                    break;
+                case "Quit":
+                    // Add Quit function
+                    break;
+                default:
+                    console.log("default");
+                    // repeat quit function
+                    break;
+            }
+        })
+
+}
