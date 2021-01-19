806ce3d95b93c4ac0ca79559497d45958fcfa1fe
Coaxial <thecoaxial@gmail.com>
2021-01-19 14:48:10 -0500

    Edited seed file

--- seeds.sql
+++ seeds.sql
@@ -25,3 +25,13 @@ CREATE TABLE employee(
     manager_id INT,
     PRIMARY KEY (id)
 )
+
+INSERT INTO employee (first_name, last_name, role_id)
+values ("Bob", "Dole", "1"), ("Richard", "Hammer", "2")
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Human Resources"), ("Legal"), ("Finance");
+
+INSERT INTO role (title, salary, Department_id)
+VALUES ("Sales Person", "50000", "1"), ("Software Engineer","60000","2"), ("HR Rep","50000","3"),("Lawyer","50000","4"),("Accountant","50000","5")
+
